4e05f0f5f7c07fa7faffbab852fce094adc745b2
artem-0ss <79014354+artem-0ss@users.noreply.github.com>
2021-06-02 18:19:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,59 +1,61 @@
-$('.radio__button1').click(function (event) {
-	$('.first__screen').toggleClass('active');
-	$('.first__screen').show();
-	$('.second__screen').hide();
-	$('.third__screen').hide();
-	$('.fourth__screen').hide();
-	$('.fifth__screen').hide();
-	$('.sixth__screen').hide();
-});
+$(document).ready(function (){
+	$('.radio__button1').click(function (event) {
+		$('.first__screen').toggleClass('active');
+		$('.first__screen').show();
+		$('.second__screen').hide();
+		$('.third__screen').hide();
+		$('.fourth__screen').hide();
+		$('.fifth__screen').hide();
+		$('.sixth__screen').hide();
+	});
 
-$('.radio__button2').click(function (event) {
-	$('.second__screen').toggleClass('active');
-	$('.second__screen').show();
-	$('.first__screen').hide();
-	$('.third__screen').hide();
-	$('.fourth__screen').hide();
-	$('.fifth__screen').hide();
-	$('.sixth__screen').hide();
-});
+	$('.radio__button2').click(function (event) {
+		$('.second__screen').toggleClass('active');
+		$('.second__screen').show();
+		$('.first__screen').hide();
+		$('.third__screen').hide();
+		$('.fourth__screen').hide();
+		$('.fifth__screen').hide();
+		$('.sixth__screen').hide();
+	});
 
-$('.radio__button3').click(function (event) {
-	$('.third__screen').toggleClass('active');
-	$('.third__screen').show();
-	$('.first__screen').hide();
-	$('.second__screen').hide();
-	$('.fourth__screen').hide();
-	$('.fifth__screen').hide();
-	$('.sixth__screen').hide();
-});
+	$('.radio__button3').click(function (event) {
+		$('.third__screen').toggleClass('active');
+		$('.third__screen').show();
+		$('.first__screen').hide();
+		$('.second__screen').hide();
+		$('.fourth__screen').hide();
+		$('.fifth__screen').hide();
+		$('.sixth__screen').hide();
+	});
 
-$('.radio__button4').click(function (event) {
-	$('.fourth__screen').toggleClass('active');
-	$('.fourth__screen').show();
-	$('.first__screen').hide();
-	$('.second__screen').hide();
-	$('.third__screen').hide();
-	$('.fifth__screen').hide();
-	$('.sixth__screen').hide();
-});
+	$('.radio__button4').click(function (event) {
+		$('.fourth__screen').toggleClass('active');
+		$('.fourth__screen').show();
+		$('.first__screen').hide();
+		$('.second__screen').hide();
+		$('.third__screen').hide();
+		$('.fifth__screen').hide();
+		$('.sixth__screen').hide();
+	});
 
-$('.radio__button5').click(function (event) {
-	$('.fifth__screen').toggleClass('active');
-	$('.fifth__screen').show();
-	$('.first__screen').hide();
-	$('.second__screen').hide();
-	$('.third__screen').hide();
-	$('.fourth__screen').hide();
-	$('.sixth__screen').hide();
-});
+	$('.radio__button5').click(function (event) {
+		$('.fifth__screen').toggleClass('active');
+		$('.fifth__screen').show();
+		$('.first__screen').hide();
+		$('.second__screen').hide();
+		$('.third__screen').hide();
+		$('.fourth__screen').hide();
+		$('.sixth__screen').hide();
+	});
 
-$('.radio__button6').click(function (event) {
-	$('.sixth__screen').toggleClass('active');
-	$('.sixth__screen').show();
-	$('.first__screen').hide();
-	$('.second__screen').hide();
-	$('.third__screen').hide();
-	$('.fourth__screen').hide();
-	$('.fifth__screen').hide();
-});
+	$('.radio__button6').click(function (event) {
+		$('.sixth__screen').toggleClass('active');
+		$('.sixth__screen').show();
+		$('.first__screen').hide();
+		$('.second__screen').hide();
+		$('.third__screen').hide();
+		$('.fourth__screen').hide();
+		$('.fifth__screen').hide();
+	});
+})
